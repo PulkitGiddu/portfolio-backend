be9a460e4d60d92f5183a5fbc6ef60380a4ebcc4
PulkitGiddu <pulkitgiddu09@gmail.com>
2026-02-15 06:01:48 +0530

    Add programmatic MailConfig

---
+++ src/main/java/com/portfolio/config/MailConfig.java
@@ -0,0 +1,37 @@
+package com.portfolio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        String username = System.getenv("MAIL_USERNAME");
+        String password = System.getenv("MAIL_PASSWORD");
+
+        System.out.println("MANUAL MAIL CONFIG: SMTP User present? " + (username != null));
+
+        if (username != null)
+            mailSender.setUsername(username);
+        if (password != null)
+            mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}

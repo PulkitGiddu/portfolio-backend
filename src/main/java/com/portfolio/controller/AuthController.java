5de0cd16ed3965ee3f6b6e0fc27cea6466b6eff4
PulkitGiddu <pulkitgiddu09@gmail.com>
2026-02-16 00:51:17 +0530

    Remove conflicting @CrossOrigin annotation from AuthController

--- src/main/java/com/portfolio/controller/AuthController.java
+++ src/main/java/com/portfolio/controller/AuthController.java
@@ -8,12 +8,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(allowCredentials = "true")
 public class AuthController {
 
     private final AppConfig appConfig;
